26029410a3f375418c5ffbb859d57ed52cec9bc8
clement_boissiere-e1 <cboissiere@apside.fr>
2023-05-24 14:22:39 +0200

    service

--- src/app/login/authentication.guard.ts
+++ src/app/login/authentication.guard.ts
@@ -1,5 +1,13 @@
-import {CanActivateFn} from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {AuthenticationService} from "./authentication.service";
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authentService = inject(AuthenticationService);
+  const router = inject(Router)
+  if (authentService.authenticated)
+    return true;
+  router.navigateByUrl('/login');
+  return false;
+
 };
