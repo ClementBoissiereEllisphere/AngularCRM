317ee21ee74fbafccc3d746d5e2e62b95c6b7194
clement_boissiere-e1 <cboissiere@apside.fr>
2023-05-24 09:34:02 +0200

    premier commit après les tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AuthenticationComponent} from './authentication/authentication.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'crm-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl('a', {
+      validators: [Validators.required, Validators.min(3)]
+    }),
+    password: new FormControl('a', {
+      validators: [Validators.required, checkPassword]
+    })
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.controls['password'].errors);
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
+}
+
+function checkPassword(c: AbstractControl): ValidatorErrors | null {
+  if (c.value.length < 5) {
+    return {
+      checkPassword: 'Error controle password'
+    };
+  }
+  return null;
+}
+
+export declare type ValidatorErrors = {
+  [key: string]: any;
+}
